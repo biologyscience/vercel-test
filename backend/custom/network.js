90af13296093e7927ad26c03e653d0ef91ee6117
RelativPerspektiv <86347995+biologyscience@users.noreply.github.com>
2025-07-02 22:25:39 +0530

    Update network.js

--- backend/custom/network.js
+++ backend/custom/network.js
@@ -2,7 +2,7 @@ const { networkInterfaces } = require('os');
 
 const interalPort = 6850;
 const externalPort = 80;
-const localIP = 'localhost';
+const localIP = '0.0.0.0';
 // const localIP = networkInterfaces()['wlan0'].filter(x => x.family === 'IPv4')[0].address;
 
 module.exports = { interalPort, externalPort, localIP };
